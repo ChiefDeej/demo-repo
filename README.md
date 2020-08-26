736635704d2528c340757098db62cad4ee62bc25
Dereck Stewart <dereckstewart@Derecks-MacBook-Pro.local>
2020-08-25 20:12:10 -0400

    Added my_file and Updated README
    demoing git commit commands

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # demo-repo
 This is a readme file. Pretty cool!
+This is a change to the readme file.
---
+++ my_file.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+#include <iostream>
+
+int main(){
+  cout << "Hello github~\n";
+
+  return 0;
+}
